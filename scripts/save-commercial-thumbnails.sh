0f926cc3797bc97e38a1f6e42bc5bc5e95402728
Dnick20 <dominic.x.lewis@gmail.com>
2025-07-22 00:36:53 -0400

    Add Tim Regus modern living room thumbnail

--- scripts/save-commercial-thumbnails.sh
+++ scripts/save-commercial-thumbnails.sh
@@ -9,4 +9,7 @@ curl -o public/images/commercial/stuttgart.jpg "https://raw.githubusercontent.co
 # Save The Lodge thumbnail (exterior building with flags)
 curl -o public/images/commercial/lodge.jpg "https://raw.githubusercontent.com/Dnick20/bv-studios-website/main/public/images/commercial/lodge.jpg"
 
+# Save Tim Regus thumbnail (modern living room with blue couch)
+curl -o public/images/commercial/tim-regus.jpg "https://raw.githubusercontent.com/Dnick20/bv-studios-website/main/public/images/commercial/tim-regus.jpg"
+
 echo "Commercial thumbnails saved to public/images/commercial/" 
